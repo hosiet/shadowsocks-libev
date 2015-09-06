5af39cfb2a793df2df581ccbbf4f37259b20f803
Boyuan Yang <073plan@gmail.com>
2015-09-06 23:23:48 +0800

    Make ss-libev dfsg-compatible.
    Originally, ss-libev is licensed under GPLv3+. However, it links
    against OpenSSL, which may cause conflict.
    
    Now we switch to PolarSSL to avoid the problem.

--- autopkg.sh
+++ autopkg.sh
@@ -6,6 +6,6 @@ git reset --hard HEAD
 autoreconf --install --verbose
 rm -f ../*.tar.xz ../*.deb ../*.tar.gz ../*.build ../*.dsc ../*.changes
 CURR_PKG_DIR=$(pwd)
-tar -czvf ../$(basename $CURR_PKG_DIR)_$(cat ./configure.ac | grep AC_INIT | grep -o '[0-9]\.[0-9]\.[0-9]').orig.tar.gz . --exclude-vcs
+tar -czvf ../$(basename $CURR_PKG_DIR)_$(cat ./configure.ac | grep AC_INIT | grep -o '[0-9]\.[0-9]\.[0-9]')+dfsg.orig.tar.gz . --exclude-vcs
 #dh_make --multi --createorig --yes --copyright gpl3
 debuild -us -uc
