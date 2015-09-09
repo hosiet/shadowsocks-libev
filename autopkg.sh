2815ec19d5670ae0613cb829c2caf9748171a06f
Boyuan Yang <073plan@gmail.com>
2015-09-09 08:27:23 +0800

    add a convenient debian packaging script
    Signed-off-by: Boyuan Yang <073plan@gmail.com>

---
+++ autopkg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+git clean -Xdf
+git reset --hard HEAD
+autoreconf --install --verbose
+rm -f ../*.tar.xz ../*.deb ../*.tar.gz ../*.build ../*.dsc ../*.changes
+CURR_PKG_DIR=$(pwd)
+tar -czvf ../$(basename $CURR_PKG_DIR)_$(cat ./configure.ac | grep AC_INIT | grep -o '[0-9]\.[0-9]\.[0-9]').orig.tar.gz . --exclude-vcs
+#dh_make --multi --createorig --yes --copyright gpl3
+debuild -us -uc
